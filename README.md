59748bc26c4746a927053740ec1fc119250f0426
Mithalesh kumar <mithaleshkumar852155@gmail.com>
2024-02-08 17:33:02 +0530

    2nd Episode

---
+++ README.md
@@ -0,0 +1,11 @@
+# parcel
+
+- Dev Build
+- Local server
+- HMR = Hot Module Reload
+- File watching algorithm - build in c++
+- Caching - Fast build
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 //  let heading=React.createElement('h1',{id:'heading'},'Hello React');
 // let root=ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
@@ -10,8 +14,8 @@
 let parent = React.createElement('div', { id: 'parent' },
     React.createElement('div', { id: 'child' },
         [
-            React.createElement('h1', {}, 'I am heading tag'),
-            React.createElement('h2', { id: 'siblingsh1' }, 'I am siblings of h1')
+            React.createElement('h1', {}, 'I am heading tag with h1'),
+            React.createElement('h2', { id: 'siblingsh1' }, 'I am siblings f h2')
         ]
     )
 );
